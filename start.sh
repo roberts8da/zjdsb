38bc7cbcd065636137b73652549f39024fedfc74
roberts8da <147053155+roberts8da@users.noreply.github.com>
2025-11-02 04:38:31 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 
-export UUID=${UUID:-'fdeeda45-0a8e-4570-bcc6-d68c995f5830'} # 如开启哪吒v1,不同的平台需要改一下，否则会覆盖
-export NEZHA_SERVER=${NEZHA_SERVER:-''}       # v1哪吒填写形式：nezha.abc.com:8008,v0哪吒填写形式：nezha.abc.com
-export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒不要填写这个,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
-export NEZHA_KEY=${NEZHA_KEY:-''}             # 哪吒v0-agent密钥或v1的NZ_CLIENT_SECRET
+export UUID=${UUID:-'0003db34-5744-4220-8603-dc5df75cfef0'} # 如开启哪吒v1,不同的平台需要改一下，否则会覆盖
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.lilyonlyone.eu.org'}       # v1哪吒填写形式：nezha.abc.com:8008,v0哪吒填写形式：nezha.abc.com
+export NEZHA_PORT=${NEZHA_PORT:-'443'}           # v1哪吒不要填写这个,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
+export NEZHA_KEY=${NEZHA_KEY:-'FTeCQhTeQht5n8FW9j'}             # 哪吒v0-agent密钥或v1的NZ_CLIENT_SECRET
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}         # 固定隧道域名,留空即启用临时隧道
 export ARGO_AUTH=${ARGO_AUTH:-''}             # 固定隧道token或json,留空即启用临时隧道
 export CFIP=${CFIP:-'cf.877774.xyz'}          # argo节点优选域名或优选ip
@@ -14,7 +14,7 @@ export ARGO_PORT=${ARGO_PORT:-'8001'}         # argo端口 使用固定隧道tok
 export CHAT_ID=${CHAT_ID:-''}                 # TG chat_id，可在https://t.me/laowang_serv00_bot 获取
 export BOT_TOKEN=${BOT_TOKEN:-''}             # TG bot_token, 使用自己的bot需要填写,使用上方的bot不用填写,不会给别人发送
 export UPLOAD_URL=${UPLOAD_URL:-''}  # 订阅自动上传地址,没有可不填,需要填部署Merge-sub项目后的首页地址,例如：https://merge.zabc.net
-export DISABLE_ARGO=${DISABLE_ARGO:-'false'}  # 是否禁用argo, true为禁用,false为不禁用
+export DISABLE_ARGO=${DISABLE_ARGO:-'true'}  # 是否禁用argo, true为禁用,false为不禁用
 
 if [ -f ".env" ]; then
     # 使用 sed 移除 export 关键字，并过滤注释行
